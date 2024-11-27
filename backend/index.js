e7dfa242d14046894b8fe68caf651d0d42fe82f9
Lucas-Guiar <marin.lucas009@gmail.com>
2024-11-27 10:00:06 -0300

    alteraçãoindex

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const userRoutes = require('./routes/userRoutes'); // Certifique-se de que o caminho está correto
+
+app.use(cors());
+
+// Middleware para interpretar JSON no corpo das requisições
+app.use(express.json());
+
+// Usa as rotas definidas no arquivo userRoutes
+app.use(userRoutes);
+
+// Inicia o servidor na porta 3000
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000');
+});
