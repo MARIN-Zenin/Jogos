fca3fc89b4c714a5667c7242f1bf0e09e891cce7
Lucas-Guiar <marin.lucas009@gmail.com>
2024-11-27 14:16:32 -0300

    routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,4 +5,13 @@ const userController = require('../controllers/userController'); // Importa o co
 // Rota GET para listar todos os usuários
 router.get('/users', userController.getUsers); 
 
+// Rota POST para criar um novo usuário
+router.post('/users', userController.createUsers); // Aqui estamos chamando o método createUser
+
+// Rota PUT para atualizar um usuário existente
+router.put('/users/:id', userController.updateUsers); // Aqui estamos chamando o método updateUser
+
+// Rota DELETE para deletar um usuário
+router.delete('/users/:id', userController.deleteUsers); // Aqui estamos chamando o método deleteUser
+
 module.exports = router;
