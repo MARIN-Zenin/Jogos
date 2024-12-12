fdb5b535cf52c0b568a6e17c4fb2097c8f0c6b7f
Zayra <Zayrafranca5@gmail.com>
2024-12-12 13:41:06 -0300

    z

--- jogos/db/db.js
+++ jogos/db/db.js
@@ -6,20 +6,12 @@ const config = {
   authentication: {
     type: "default", // Tipo de autenticação (no caso, autenticando com nome de usuário e senha)
     options: {
-<<<<<<< HEAD
       userName: "testeqsl", // Nome do usuário do banco de dados
-=======
-      userName: "testeSQL", // Nome do usuário do banco de dados
->>>>>>> 276e5b21604fe67e54139af6d7acfbad4e7540c2
       password: "12345", // Senha do banco de dados
     },
   },
   options: {
-<<<<<<< HEAD
     database: "USERS", // Nome do banco de dados
-=======
-    database: "projetoJogos", // Nome do banco de dados
->>>>>>> 276e5b21604fe67e54139af6d7acfbad4e7540c2
     encrypt: false, // Desativar criptografia (pode mudar se necessário)
     port: 1433, // Porta padrão do SQL Server
     trustServerCertificate: true, // Necessário para evitar erro de SSL em ambiente local
